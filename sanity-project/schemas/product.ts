156d256a441f92fdd28504f4adc11ea9d2a8fd67
tejasvising <85549686+tejasvising@users.noreply.github.com>
2023-09-08 15:56:08 +0530

    main code

---
+++ sanity-project/schemas/product.ts
@@ -0,0 +1,44 @@
+export default{
+    name:'product',
+    title:'Product',
+    type:'document',
+    fields:[
+        {
+            name:'name',
+            type:'string',
+            title:'Name',
+        },
+        {
+            name:'price',
+            type:'number',
+            title:'Price',
+        },
+        {
+            name:'slug',
+            type:'slug',
+            title:'Slug',
+            options:{
+                source:'name'
+            }
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'text',
+        },
+        {
+            title:'Stripe Product Id',
+            name:'stripeProductId',
+            type:'string',
+        },
+        {
+            name:'image',
+            type:'array',
+            title:'Image',
+            of:[{type:'image'}],
+            options: {
+                hotspot:true,
+            },
+        },
+    ],
+}
